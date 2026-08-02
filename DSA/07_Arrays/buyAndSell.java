ff3d00f379e9362ec3f86f33bce41cd8c1241c88
viswateja-2007 <viswateja6140@gmail.com>
2026-08-02 12:08:51 +0530

    Added buyAndSell problem

--- DSA/07_Arrays/buyAndSell.java
+++ DSA/07_Arrays/buyAndSell.java
@@ -2,7 +2,7 @@ public class buyAndSell{
   public static int buyAndSell(int arr[]){
 
     int buy = Integer.MAX_VALUE;
-    int profitMax = Integer.MIN_VALUE;
+    int profitMax = 0;
     int profit = 0;
 
     for(int i = 0; i < arr.length; i++){
@@ -22,13 +22,9 @@ public class buyAndSell{
   }
 
   public static void main(String args[]){
-    int prices[] = {7,2,5,3,6,1};
-    int a1 = buyAndSell(prices);
-    System.out.println(a1);
-
-
-
-
+    int prices[] = {7, 1, 5, 3, 6, 4};
+    int profit = buyAndSell(prices);
+    System.out.println("profit is : " + profit);
 
   }
 
